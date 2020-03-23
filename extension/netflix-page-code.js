6af9db3c67a5151e7b493c9f1b7c52b42395f0fe
Deb <debkbanerji@gmail.com>
2020-03-22 22:21:48 -0700

    URL Param checks

--- extension/netflix-page-code.js
+++ extension/netflix-page-code.js
@@ -5,27 +5,38 @@ function embeddedCode() {
     STARTED: 'started'
   }
 
+  // Have to define constants in this function
+
   const TIME_BEFORE_RUN = 1000; // Give Netflix this much time to load
   // TODO: Do this reactively rather than guessing
 
+  const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestamp';
+  const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
+
+  const SYNC_VIDEO_TIMESTAMP_PARAM = 'syncVideoTimestamp';
+  const SYNC_VIDEO_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_VIDEO_TIMESTAMP_PARAM + "=([^&#]*)");
+
+
   function getPlayer() {
+    console.log(window);
+
     try {
       const videoPlayer = netflix
         .appContext
         .state
         .playerApp
         .getAPI()
-        .videoPlayer
+        .videoPlayer;
 
       // Getting player id
       const playerSessionId = videoPlayer
-        .getAllPlayerSessionIds()[0]
+        .getAllPlayerSessionIds()[0];
 
       const player = videoPlayer
-        .getVideoPlayerBySessionId(playerSessionId)
+        .getVideoPlayerBySessionId(playerSessionId);
 
-      return player
-    } catch(err) {
+      return player;
+    } catch (err) {
       alert("Netflix link sync unable to access player on page")
       console.error(err);
     }
@@ -49,7 +60,6 @@ function embeddedCode() {
   setTimeout(function() {
     const player = getPlayer();
     player.pause();
-    console.log(player)
     player.seek(1091243) //seek to roughly 18mins
     // player.play();
   }, TIME_BEFORE_RUN * 3);
@@ -64,4 +74,18 @@ function embedInPage(fn) {
   document.documentElement.appendChild(script);
 }
 
-embedInPage(embeddedCode);
+
+// Define these ones here as well since we need to use these to check whether
+// or not we need to embed code in the first place
+const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestamp';
+const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
+const SYNC_VIDEO_TIMESTAMP_PARAM = 'syncVideoTimestamp';
+const SYNC_VIDEO_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_VIDEO_TIMESTAMP_PARAM + "=([^&#]*)");
+
+const url = window.location.href;
+
+// Only embed the code in the page if both parameters exist
+// Ex: https://www.netflix.com/watch/70079583?syncGMTTimestamp=1584939579?syncVideoTimestamp=1091243
+if (SYNC_GMT_TIMESTAMP_REGEX.test(url) && SYNC_VIDEO_TIMESTAMP_REGEX.test(url)) {
+  embedInPage(embeddedCode);
+}
