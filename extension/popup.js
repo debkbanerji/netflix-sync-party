e6c734bd43395412d94cebd30282aa0dcc09b5e2
Deb <debkbanerji@gmail.com>
2020-03-23 22:32:32 -0700

    Resync button

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,10 @@
-const NETFLIX_WATCH_REGEX = /netflix\.com\/watch\/\d*/gi
-const WATCH_TRACK_REGEX = /watch\/\d*/gi
-const TRACK_ID_REGEX = /\d.*/gi
-const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestampSec';
-const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
-
-
 function runOnNetflixTab(tab) {
+  const NETFLIX_WATCH_REGEX = /netflix\.com\/watch\/\d*/gi
+  const WATCH_TRACK_REGEX = /watch\/\d*/gi
+  const TRACK_ID_REGEX = /\d.*/gi
+  const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestampSec';
+  const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
+
   const url = tab.url;
 
   // hide all possible views, then show the one we want to view
@@ -15,16 +14,22 @@ function runOnNetflixTab(tab) {
 
   let trackID = null;
 
+  console.log(url);
+
   if (NETFLIX_WATCH_REGEX.test(url)) {
     if (SYNC_GMT_TIMESTAMP_REGEX.test(url)) {
       document.getElementById('synced-video-view').hidden = false;
+
+      document.getElementById('resync').addEventListener('click', () => {
+        chrome.tabs.reload(tab.id);
+      });
+
     } else {
       document.getElementById('unsynced-video-view').hidden = false;
 
       // get track ID
       const match = WATCH_TRACK_REGEX.exec(url)[0];
       const trackID = TRACK_ID_REGEX.exec(match)[0];
-      
     }
   } else {
     document.getElementById('non-video-view').hidden = false;
