1f4d104a6853da874b487edc5a5b151dcc9777c6
Deb <debkbanerji@gmail.com>
2020-03-23 22:00:12 -0700

    Getting track id from url

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,6 @@
 const NETFLIX_WATCH_REGEX = /netflix\.com\/watch\/\d*/gi
+const WATCH_TRACK_REGEX = /watch\/\d*/gi
+const TRACK_ID_REGEX = /\d.*/gi
 const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestampSec';
 const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
 
@@ -11,11 +13,18 @@ function runOnNetflixTab(tab) {
   document.getElementById('unsynced-video-view').hidden = true;
   document.getElementById('non-video-view').hidden = true;
 
+  let trackID = null;
+
   if (NETFLIX_WATCH_REGEX.test(url)) {
     if (SYNC_GMT_TIMESTAMP_REGEX.test(url)) {
       document.getElementById('synced-video-view').hidden = false;
     } else {
       document.getElementById('unsynced-video-view').hidden = false;
+
+      // get track ID
+      const match = WATCH_TRACK_REGEX.exec(url)[0];
+      const trackID = TRACK_ID_REGEX.exec(match)[0];
+      
     }
   } else {
     document.getElementById('non-video-view').hidden = false;
