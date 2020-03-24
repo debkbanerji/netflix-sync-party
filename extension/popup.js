c282f5d3f2f19d4d3f54bf5ab8d9168aff8d86c3
Deb <debkbanerji@gmail.com>
2020-03-23 20:16:12 -0700

    Logic to select section

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,25 @@
+const NETFLIX_WATCH_REGEX = /netflix\.com\/watch\/\d*/gi
+const SYNC_GMT_TIMESTAMP_PARAM = 'syncGMTTimestampSec';
+const SYNC_GMT_TIMESTAMP_REGEX = new RegExp("[\\?&]" + SYNC_GMT_TIMESTAMP_PARAM + "=([^&#]*)");
+
+
 function runOnNetflixTab(tab) {
   const url = tab.url;
-  console.log(url);
+
+  // hide all possible views, then show the one we want to view
+  document.getElementById('synced-video-view').hidden = true
+  document.getElementById('unsynced-video-view').hidden = true;
+  document.getElementById('non-video-view').hidden = true;
+
+  if (NETFLIX_WATCH_REGEX.test(url)) {
+    if (SYNC_GMT_TIMESTAMP_REGEX.test(url)) {
+      document.getElementById('synced-video-view').hidden = false;
+    } else {
+      document.getElementById('unsynced-video-view').hidden = false;
+    }
+  } else {
+    document.getElementById('non-video-view').hidden = false;
+  }
 }
 
 chrome.tabs.query({
